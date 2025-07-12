1d69bf408d7182d30c5a46c56e19861e04a29a49
iru <study.iru@gmail.com>
2025-07-13 05:10:45 +0900

    정적 페이지 배포

--- svelte.config.js
+++ svelte.config.js
@@ -8,18 +8,22 @@ const config = {
     preprocess: vitePreprocess({
         // PostCSS 설정을 추가합니다.
         // 이는 Sass가 Node.js 모듈을 가져올 수 있도록 돕습니다.
-        script: true,
-        style: true,
-        postcss: true, // PostCSS 설정도 유지,
-        scss: {
-            // additionalData: `@import "@/styles/variables.scss";` // 파일명 확인 (variables.scss)
-        }
+        // script: true,
+        // style: true,
+        // postcss: true, // PostCSS 설정도 유지,
+        // scss: {
+        //     // additionalData: `@import "@/styles/variables.scss";` // 파일명 확인 (variables.scss)
+        // }
     }),
 
     kit: {
         adapter: adapter(),
-        alias: { // 이 부분 추가
-            '@': path.resolve('./src') // 프로젝트 루트의 src 폴더를 '@'로 설정
+        paths: { // GitHub Pages에 배포할 때 중요한 부분!
+            base: process.env.NODE_ENV === 'production' ? '/point_math_svelte' : '', // 프로덕션 환경에서만 기본 경로 설정
+                                                                               // '당신의-저장소-이름'을 실제 GitHub 저장소 이름으로 변경
+        },
+        alias: {
+            '@': path.resolve('./src')
         }
     }
 };
