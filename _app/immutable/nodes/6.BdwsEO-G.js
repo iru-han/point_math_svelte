4be61e94f45ff7eff893a1337a92158ce3a99c2e
iru-han <iru-han@users.noreply.github.com>
2025-07-14 08:55:34 +0000

    deploy: c0b92c7dda25860446f896da5808444ede06974b

--- _app/immutable/nodes/6.BdwsEO-G.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/NZTpNUN0.js";import"../chunks/C2mA3Dd9.js";import{B as t}from"../chunks/CrOMXhH_.js";function r(o){t(o,{boardType:"notice"})}export{r as component};
---
+++ _app/immutable/nodes/8.CZcHxQQa.js
@@ -0,0 +1,1 @@
+import"../chunks/NZTpNUN0.js";import"../chunks/C2mA3Dd9.js";import{B as r}from"../chunks/CfoUwv5x.js";function a(o){r(o,{boardType:"recruit"})}export{a as component};
